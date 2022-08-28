c4f2d1f8c93576c15d525f7ca9d17266f638072a
Westborn <george@westborn.com.au>
2022-08-28 18:49:33 +1000

    code for enrolment maths

--- server/code.js
+++ server/code.js
@@ -6,18 +6,25 @@ this.CONFIG = {
   },
 }
 
-function testget() {
-  const ss = SpreadsheetApp.openById(CONFIG.TERM_SHEET)
-  const res = getCourseDetails('', ss)
-  console.log(res.getContent())
-}
-
 function testadd() {
   const ss = SpreadsheetApp.openById(CONFIG.TERM_SHEET)
   const res = addEnrolments(
-    { name: 'George Stone', email: 'george@westborn.com.au', coursesEnrolled: ['testing', 'tester'] },
+    {
+      name: 'George1 Stone',
+      email: 'george1@westborn.com.au',
+      coursesEnrolled: [
+        { title: 'testing', status: 'Enrol?' },
+        { title: 'tester', status: 'Waitlist?' },
+      ],
+    },
     ss
   )
+  console.log(res.getContent())
+}
+
+function testget() {
+  const ss = SpreadsheetApp.openById(CONFIG.TERM_SHEET)
+  const res = getCourseDetails('', ss)
   console.log(res.getContent())
 }
 
@@ -59,8 +66,7 @@ function doPost(e) {
 
 function getCourseDetails(request, ss) {
   //now get the details for all courses
-  var courseSheet = ss.getSheetByName('CourseDetails')
-  const courseData = courseSheet.getDataRange().getValues()
+  const courseData = ss.getSheetByName('CourseDetails').getDataRange().getValues()
   const allCourses = wbLib.getJsonArrayFromData(courseData)
   return sendResponse('ok', { data: allCourses })
 }
@@ -75,11 +81,7 @@ function addEnrolments(request, ss) {
   if (!request.coursesEnrolled || request.coursesEnrolled.length === 0) {
     return sendResponse('error', 'No courses fror enrolment')
   }
-  //get courseDetail sheet
-  const courseData = ss.getSheetByName('CourseDetails').getDataRange().getValues()
-  const allCourses = wbLib.getJsonArrayFromData(courseData)
-
-  var enrolmentSheet = ss.getSheetByName('OnlineEnrolments')
+  const enrolmentSheet = ss.getSheetByName('OnlineEnrolments')
 
   //map the enrolments to an array of ["name", "email", courseEnroled]
   const enrolments = request.coursesEnrolled.map((course) => [
@@ -90,20 +92,60 @@ function addEnrolments(request, ss) {
     course.status,
   ])
 
-  //write the enrolments for this member to the sheet
+  //write all the enrolments for this member to the sheet
   enrolmentSheet
     .getRange(enrolmentSheet.getLastRow() + 1, 1, enrolments.length, enrolments[0].length)
     .setValues(enrolments)
 
   //set a formula in the last 2 columns as error checking
-  const formulas = [
-    'ArrayFormula(index(Members,match(TRUE, exact(B2,memberName),0),1))',
-    'ArrayFormula(index(Members,match(TRUE, exact(C2,memberEmail),0),1))',
-  ]
-  enrolmentSheet.getRange(2, 6, 1, 2).setFormulas([formulas])
-  const fillDownRange = enrolmentSheet.getRange(2, 6, enrolmentSheet.getLastRow() - 1)
-  enrolmentSheet.getRange(2, 6, 1, 2).copyTo(fillDownRange)
+  const headers = enrolmentSheet.getRange(1, 1, 1, enrolmentSheet.getLastColumn()).getValues().shift()
+  const nameCheckCol = headers.indexOf('nameCheck') + 1
+  const emailCheckCol = headers.indexOf('emailCheck') + 1
+  const lastRow = enrolmentSheet.getLastRow() - 1
+  var fillDownRange
 
+  // nameCheck formula
+  enrolmentSheet
+    .getRange(2, nameCheckCol, 1, 1)
+    .setFormula('=ArrayFormula(index(Members,match(TRUE, exact(B2,memberName),0),1))')
+  fillDownRange = enrolmentSheet.getRange(2, nameCheckCol, lastRow)
+  enrolmentSheet.getRange(2, nameCheckCol, 1, 1).copyTo(fillDownRange)
+
+  // emailCheck formula
+  enrolmentSheet
+    .getRange(2, emailCheckCol, 1, 1)
+    .setFormula('=ArrayFormula(index(Members,match(TRUE, exact(C2,memberEmail),0),1))')
+  fillDownRange = enrolmentSheet.getRange(2, emailCheckCol, lastRow)
+  enrolmentSheet.getRange(2, emailCheckCol, 1, 1).copyTo(fillDownRange)
+
+  // get all the enrolments to-date
+  const enrolmentData = enrolmentSheet.getDataRange().getValues()
+  const allEnrolments = wbLib.getJsonArrayFromData(enrolmentData)
+
+  // sum them by enrolment type (enrol or waitlist)
+  const occurrences = allEnrolments
+    .reduce((acc, curr, idx) => {
+      if (acc[curr.courseEnrolledIn]) {
+        ++acc[curr.courseEnrolledIn][curr.status === 'Enrol?' ? 'enrol' : 'waitlist']
+      } else {
+        acc[curr.courseEnrolledIn] = { enrol: curr.status === 'Enrol?' ? 1 : 0, waitlist: curr.status === 'Waitlist?' ? 1 : 0 }
+      }
+      return acc
+    }, {})
+  console.log(Object.keys(occurrences))
+  console.log(Object.values(occurrences))
+  console.log(Object.entries(occurrences))
+  console.log(Object.values(occurrences).reduce((acc, {enrol, waitlist}) => {return acc+ enrol + waitlist}, 0))
+
+  const a = Object.entries(occurrences).map(([title, { enrol, waitlist }]) => { return ({ title: title, total: enrol + waitlist }) })
+  console.log(JSON.stringify(a, null, 2))
+
+  //get courseDetail sheet
+  const courseData = ss.getSheetByName('CourseDetails').getDataRange().getValues()
+  const allCourses = wbLib.getJsonArrayFromData(courseData)
+  const courseHeaders = courseData.shift()
+
+  // we're done here
   return sendResponse('ok', { data: enrolments })
 }
 
