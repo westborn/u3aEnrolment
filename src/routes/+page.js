2fa81d4179457f2c3984efeccffc2bd769e41061
George Stone <george@westborn.com.au>
2024-01-29 11:51:21 +1100

    Update all packages to latest

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,7 @@
 export async function load({ fetch }) {
   // console.log('Sending')
   const response = await fetch(
-    'https://script.google.com/macros/s/AKfycbyv5w_lYQUTGG1Kh_TfXpwgiOedmKGvc74nIJYCJqdwZlbAP0BglOnruwOkykqqlmshbw/exec?requestType=getCourseDetail',
+    'https://script.google.com/macros/s/AKfycbz-XGsvHwrqXyjQ_IRGt-Z64N7vhnEA-vgq3QTUn_hG9mteLi05C46CCbeTfm_4zhf8ww/exec?requestType=getCourseDetail',
     { method: 'POST', body: JSON.stringify({ action: 'get_data' }) }
   )
   const courseDetailResponse = await response.json()
