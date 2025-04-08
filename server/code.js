cef654b85122ec9efea1d1a7b9cd9f12a0091958
George Stone <george@westborn.com.au>
2025-04-08 15:34:28 +1000

    Add email verification for membership status in enrolment process

--- server/code.js
+++ server/code.js
@@ -56,6 +56,17 @@ function validateEmail(mail) {
   return false
 }
 
+function isMemberEmailKnown(email) {
+  const memberData = ss.getSheetByName('MemberDetails').getDataRange().getValues()
+  const allMembers = wbLib.getJsonArrayFromData(memberData)
+
+  const memberEmail = allMembers.find((member) => member.email === email)
+  if (memberEmail) {
+    return true
+  }
+  return false
+}
+
 function addEnrolments(request, ss) {
   if (!request.name || request.name.trim() === '') {
     return sendResponse('error', 'Invalid name for enrolment')
@@ -142,9 +153,17 @@ function addEnrolments(request, ss) {
 
   const paymentReceipt = request.paymentReceipt ? `<p>Payment Receipt: ${request.paymentReceipt}</p><br>` : ''
 
+  const knownEmailMessage = isMemberEmailKnown(request.email)
+    ? `<p>We have your email address on file.</p><br>`
+    : `
+<p>Please be aware that we were not able to adequately verify your email address.
+<br>Your membership may have lapsed, or you may not be a member yet. (or we may have slipped up somewhere)
+<br>We will contact you directly to try to resolve your membership, however,<br>you have been enrolled/waitlisted for the courses shown below.
+<br></p>`
+
   const fieldReplacer = {
     memberName: request.name,
-    classInfo: enrolHTML + paymentReceipt + waitlistHTML,
+    classInfo: enrolHTML + paymentReceipt + waitlistHTML + knownEmailMessage,
   }
   // get the draft Gmail message to use as a template
   const emailTemplate = wbLib.getGmailTemplateFromDrafts('TEMPLATE - Course Enrolment Confirmation')
